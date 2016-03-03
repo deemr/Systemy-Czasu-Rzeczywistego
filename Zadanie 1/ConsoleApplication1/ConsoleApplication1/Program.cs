8d22a5b2efe67f39daab812a251b8efff9abcc36
Rafal <deemr94@gmail.com>
2016-03-03 22:43:27 +0100

    Add files from lab on 03.03.2016

---
+++ Zadanie 1/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+
+    abstract class Agent : Interface1
+    {
+
+        abstract public void Update();
+
+        IEnumerator Interface1.CoroutineUpdate()
+        {
+            throw new NotImplementedException();
+        }
+
+        void Interface1.Run()
+        {
+
+            while (HasFinished != true)
+            {
+
+                Thread.Sleep(100);
+                this.Update();
+
+            }
+
+        }
+
+        public bool HasFinished;
+
+    }
+
+    class AgentKonkretny : Agent
+    {
+
+        private int agentId;
+        private int Licznik;
+
+        public override void Update()
+        {
+
+
+            Console.WriteLine("Agent o id: " + agentId + ", wartosc licznika: " + Licznik);
+            Licznik++;
+            this.HasFinished = true;
+
+
+        }
+
+        public AgentKonkretny(int agentId)
+        {
+            Licznik = 0;
+            this.agentId = agentId;
+
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Interface1 agent = new AgentKonkretny(0);
+            Interface1 agent1 = new AgentKonkretny(1);
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
